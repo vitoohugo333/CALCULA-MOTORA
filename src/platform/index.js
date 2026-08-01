123c45be82e1965bf5452f8504ce499ecbb87f4e
vitoohugo333 <hugovitorslipknot@gmail.com>
2026-08-01 20:44:07 -0300

    feat(platform): export platform foundation

---
+++ src/platform/index.js
@@ -0,0 +1,5 @@
+export { createDiagnostics } from './diagnostics.js';
+export { createEventBus } from './event-bus.js';
+export { createFeatureFlags } from './feature-flags.js';
+export { defineModule, defineModuleManifest, MODULE_STATUSES } from './manifest.js';
+export { createModuleRegistry } from './module-registry.js';
